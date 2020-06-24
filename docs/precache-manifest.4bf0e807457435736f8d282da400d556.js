bbe21e4b654dd8b6e122ed8fe507773d5159ba09
Luis <luismiguel0020@gmail.com>
2020-06-23 21:33:42 -0500

    added box

---
+++ docs/precache-manifest.4bf0e807457435736f8d282da400d556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d106be74ea0eeba2155858697f4a2ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73c2ed4913fe908cd37e",
+    "url": "/static/css/main.4d36f1bc.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c2ed4913fe908cd37e",
+    "url": "/static/js/main.d45274f2.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
